ee63135d1c3e552dbe5a79fb22d5559fc4d4be5b
Brandon <brandon15811@gmail.com>
2011-01-30 13:35:07 -0600

    initial branch commit

--- xml.php
+++ xml.php
@@ -1,7 +1,7 @@
 <?php
 $ip = $_COOKIE['ip'];
 $ch=curl_init();
-curl_setopt($ch,CURLOPT_URL,"http://".$ip.":8060/query/apps");
+curl_setopt($ch,CURLOPT_URL,"http://".$ip.":8080/query/apps");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 $xml = curl_exec($ch); 
 curl_close($ch);
