8a84ae73a82f8112a06f55f7da2361817a18a768
Brandon <brandon15811@gmail.com>
2011-02-06 20:15:38 -0600

    more ssdp changes

--- ssdp.php
+++ ssdp.php
@@ -44,6 +44,7 @@ if($opt_ret < 0)
 	echo "socket_set_option() failed, error: " . strerror($opt_ret);
 }
 $i = 6;
+$jsona = array();
 while ($i > 0)
 {
 	$send_ret = socket_sendto($sock, $bc_string, strlen($bc_string), 0, '239.255.255.250', 1900);
@@ -53,20 +54,30 @@ while ($i > 0)
 		break;
 	} else {
 		$input = socket_read($sock, 2048);
-		$jsona=array();
 		if (strstr($input, "roku:ecp"))
 		{
 			$header = http_parse_headers($input);
-			print_r($header);
-			$uuid = $header['USN'];
-			$uuid = str_replace("uuid:roku:ecp", "", $uuid);
+			/* echo "<pre>";
+			//print_r($header);
+			echo "</pre><br>"; */
+			$uuid = $header['Usn'];
+			$uuid = str_replace("uuid:roku:ecp:", "", $uuid);
+			// var_export($uuid);
 			$ip = parse_url($header['Location']);
-			$jsona[$uuid] = $ip['host'].$ip['port'];
+			if (!array_key_exists($uuid, $jsona))
+			{
+			$jsont = array($uuid => $ip['host'].":".$ip['port']);
+			$jsona = array_merge($jsont, $jsona);
+			}
+			/* echo "<pre>";
+			print_r($ip);
+			echo "</pre><br>"; */
 		}
 		sleep(1);
 	}
 	$i--;
 }
 socket_close($sock);
-print_r($jsona);
+echo json_encode($jsona);
+// print_r($jsona);
 ?>
