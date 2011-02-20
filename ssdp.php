cf16338cf959dc5fa3d732b1f4d7247e9d62e2af
Brandon <brandon15811@gmail.com>
2011-02-20 16:36:08 -0600

    43r5

--- ssdp.php
+++ ssdp.php
@@ -78,6 +78,102 @@ while ($i > 0)
 	$i--;
 }
 socket_close($sock);
-echo json_encode($jsona);
+//echo json_encode($jsona);
 // print_r($jsona);
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta content="index,follow" name="robots" />
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+<link href="pics/homescreen.png" rel="apple-touch-icon" />
+<meta name = "viewport" content = "user-scalable=no, width=device-width">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script src="javascript/functions.js" type="text/javascript"></script>
+<script src="cookie.js" type="text/javascript"></script>
+<script type="text/javascript">
+function gup( name )
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return results[1];
+}
+if (gup('ip'))
+	{
+	setCookie('ip',gup('ip'),3650);
+	ip=getCookie('ip');
+	}
+else if (!checkCookie())
+	{
+	window.location='config.html'
+	}
+else
+	{
+	ip=getCookie('ip');
+	}
+function press(type,cmd)
+{
+	xmlhttp=new XMLHttpRequest();
+	if (type=="query")
+	{
+		php=getCookie('php')
+		if (php="on")
+		{
+			console.log(php)
+			url="xml.php"
+		} else {
+			url="http://"+ip+":8060/" + type + "/" + cmd
+		}
+		xmlhttp.open("GET",url,true);
+		xmlhttp.onreadystatechange=function()
+		{
+			if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+			parser=new DOMParser();
+			xmlDoc=parser.parseFromString(xmlhttp.responseText,"text/xml");
+			applist(xmlDoc);
+			}
+		}
+	xmlhttp.send();
+	}
+	else
+	{
+	xmlhttp.open("POST","http://"+ip+":8060/" + type + "/" + cmd,true);
+	xmlhttp.send();
+	}
+}
+</script>
+</head>
+
+
+<body>
+<div id="topbar" class="black">
+	<div id="title">Roku Remote</div>
+	<div id="leftnav">
+		<a href="index.html"><img alt="home" src="images/home.png" /></a></div>
+	<div id="rightbutton">
+		<a href="config.html">Config</a></div>
+</div>
+
+
+<div id="content">
+<span class="graytitle">Players</span>
+<ul class="pageitem" id='applist'>
+<?php
+while (list($sn, $ip) = each($jsona)) {
+    //echo "Key: $key; Value: $value<br />\n";
+	//var_dump($player);
+	$ip = str_replace(":8060", "", $ip);
+	echo "<li class='menu'><a href='index.html?ip=".$ip."'><span class='name'>".$ip."</span><span class='arrow'></span></a></li>";
+}
+?>
+</ul>
+</div>
+</body>
+</html>
